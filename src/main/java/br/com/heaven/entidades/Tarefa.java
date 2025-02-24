c8365dfb81405695b821ec68c6503bc4e4bae52a
ancelemarcos <102827898+ancelemarcos@users.noreply.github.com>
2025-02-24 10:08:50 -0300

    Update Tarefa.java

--- src/main/java/br/com/heaven/entidades/Tarefa.java
+++ src/main/java/br/com/heaven/entidades/Tarefa.java
@@ -3,40 +3,47 @@ package br.com.heaven.entidades;
 import javax.servlet.ServletContext;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Tarefa implements Runnable {
-    String tarefaNome;
-    long tarefaTempo;
-    ServletContext sce;
+    private static final Logger logger = Logger.getLogger(Tarefa.class.getName());
+    private static final long MILLISECONDS_MULTIPLIER = 1000L;
+    
+    private final String tarefaNome;
+    private final long tarefaTempo;
+    private final ServletContext sce;
+    
     public Tarefa(int tarefaNumero, int multiplicador, ServletContext pSce) {
         this.tarefaNome = "Tarefa n. " + String.valueOf(tarefaNumero);
-        this.tarefaTempo = 1000L * multiplicador;
+        this.tarefaTempo = MILLISECONDS_MULTIPLIER * multiplicador;
         this.sce = pSce;
     }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        logger.info("VAI ESPERAR...> " + Thread.currentThread().getName());
         ExecutorService executor = (ExecutorService) this.sce.getAttribute("executorService");
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
         long y = 1;
         try {
-            System.out.println("VAI ESPERAR..> " + this.tarefaTempo);
+            logger.info("VAI ESPERAR..> " + this.tarefaTempo);
             Thread.sleep(this.tarefaTempo);
             for (long x = 1; x < 1000000; x++) {
                 if (y==1000||x==999999) {
-                    //System.out.println(Thread.currentThread().getName() + " -> " + String.valueOf(x));
+                    //logger.info(Thread.currentThread().getName() + " -> " + String.valueOf(x));
                     y = 1;
                 }else{
                     y++;
                 }
             }
-            System.out.println(Thread.currentThread().getName());
-            System.out.println("Threads em uso...> " + threadPoolExecutor.getActiveCount());
-            System.out.println("Threads ativas: " + threadPoolExecutor.getActiveCount());
-            System.out.println("Tarefas completadas: " + threadPoolExecutor.getCompletedTaskCount());
+            logger.info(Thread.currentThread().getName());
+            logger.info("Threads em uso...> " + threadPoolExecutor.getActiveCount());
+            logger.info("Threads ativas: " + threadPoolExecutor.getActiveCount());
+            logger.info("Tarefas completadas: " + threadPoolExecutor.getCompletedTaskCount());
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            logger.log(Level.SEVERE, "Essa Thread foi interrompida!");
+            Thread.currentThread().interrupt();
         }
     }
 }
